e92fbca09334a8d1556179fe4c10937ed2f3f61f
justdprroz <dmitri.proskuriakov@gmail.com>
2021-07-26 17:12:12 +0300

    added build script for windows and changed configuration to relwithdebinfo

---
+++ buildWin.bat
@@ -0,0 +1,17 @@
+@echo off
+rem Generate directories
+
+if not exist build\ mkdir build
+if not exist tmp\ mkdir tmp
+
+rem Go to directories
+
+cd tmp
+
+rem Generate cmake files
+
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+
+rem Build projects
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" ".\Twilight Escapers.sln"
