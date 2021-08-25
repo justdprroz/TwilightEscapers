19d1b06ca15385aec903ed915b7de8ead23b420d
justdprroz <dmitri.proskuriakov@gmail.com>
2021-08-25 15:01:54 +0300

    some fetures

--- source/main.cpp
+++ source/main.cpp
@@ -2,17 +2,30 @@
 #include <iostream>
 #include "utils.hpp"
 #include "gui.hpp"
+#include "noise.hpp"
+
+const siv::PerlinNoise perlin(time(NULL));
+int const MAPsize = 64;
+float scale = 1;
+int MAP[MAPsize][MAPsize];
+sf::Texture textures[16];
+std::pair<int, int> playerPos = {100, 200};
+sf::Texture playerTexture;
 
 int clicked(){
 	std::cout << "clicked" << "\n";
-	state = 1;
+	state = 2;
 	return 0;
 }
 
-sf::Texture textures[16];
-
 void loadTextures(){
-	textures[0] = sf::Texture();
+	std::cout << "Loading textures:\n";
+	std::cout << "Loading tiles\n";
+	std::cout << "Stone: " << textures[0].loadFromFile("textures/tiles/normal/stone.png") << '\n';
+	std::cout << "Grass: " << textures[1].loadFromFile("textures/tiles/normal/grass.png") << '\n';
+	std::cout << "loading entities textures\n";
+	std::cout << "Guard: " << playerTexture.loadFromFile("textures/entities/guard.png") << '\n';
+	std::cout << "Textures loaded\n";
 }
 
 void MainMenu(sf::RenderWindow &win) {
@@ -31,8 +44,8 @@ void MainMenu(sf::RenderWindow &win) {
 	button(bc);
 
 	sf::Texture backgroundTexture;
-	backgroundTexture.loadFromFile("textures/background.png");
-	backgroundTexture.setSmooth(1);
+	// backgroundTexture.loadFromFile("textures/background.png");
+	// backgroundTexture.setSmooth(1);
 
 	sf::Sprite background;
 	background.setTexture(backgroundTexture);
@@ -49,25 +62,45 @@ void DebugScreen(sf::RenderWindow &win){
 	win.draw(cell);
 }
 
-int MAP[16][16];
+
+void drawEntities(sf::RenderWindow &win){
+		sf::Sprite cell;
+		cell.setTexture(playerTexture);
+		cell.setPosition(playerPos.first, playerPos.second);
+		cell.setScale(sf::Vector2f(scale,scale));
+		win.draw(cell);
+}
+
+void drawTiles(sf::RenderWindow &win) {
+	for(int i = 0; i < MAPsize; i++){
+		for(int i1 = 0; i1 < MAPsize; i1++){
+			int curBlock = MAP[i][i1];
+			sf::Sprite cell;
+			cell.setTexture(textures[curBlock]);
+			cell.setPosition(i * 16 * scale, i1 * 16 * scale);
+			cell.setScale(sf::Vector2f(scale,scale));
+			win.draw(cell);
+		}
+	}
+}
 
 void GameScreen(sf::RenderWindow &win){
-	for(int i = 0; i < 16; i++){
-		for(int i1 = 0; i1 < 16; i1++){
-			int curBlock = MAP[i][i1];
-			if (curBlock == 1){
+	drawTiles(win);
+	drawEntities(win);
+}
 
-			}
+void generateMap(){
+	for(int i = 0; i < MAPsize; i++){
+		for(int i1 = 0; i1 < MAPsize; i1++){
+			int noise = (int)(perlin.accumulatedOctaveNoise2D_0_1(i * 4.0 / MAPsize, i1 * 4.0 / MAPsize, 8) * 32) % 2;
+			MAP[i][i1] = noise;
 		}
 	}
 }
 
 int main(){
-	for(int i = 0; i < 16; i++){
-		for(int i1 = 0; i1 < 16; i1++){
-			MAP[i][i1] = 1;
-		}
-	}
+	loadTextures();
+	generateMap();
 	int width = 1000, height = 1000;
 	sf::RenderWindow window(sf::VideoMode(width, height), "EscapeFromTwilight", sf::Style::Default);
 	window.setPosition({0,0});
@@ -86,9 +119,10 @@ int main(){
 				return 0;
 			}
 			if (event.type == sf::Event::Resized) {
+				int scale = 1;
 				width = event.size.width;
 				height = event.size.height;
-				view.setSize(sf::Vector2f(width, height));
+				view.setSize(sf::Vector2f(width * scale, height * scale));
 				view.setCenter(sf::Vector2f(width / 2, height / 2));
 				window.setView(view);
 			}
@@ -99,7 +133,9 @@ int main(){
 				}
 			}
 			if (event.type == sf::Event::KeyPressed){
-
+				if (event.key.code == sf::Keyboard::W){
+					
+				}
 			}
 
 			if (event.type == sf::Event::MouseMoved){
@@ -114,7 +150,7 @@ int main(){
 			DebugScreen(window);
 		}
 		if (state == 2){
-			MainMenu(window);
+			GameScreen(window);
 		}
 		window.display();
     }
